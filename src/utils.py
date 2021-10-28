2219c40e9adc0ec8b999384ccb5395f5964030b3
avetter1998 <avetter1998@gmail.com>
2021-10-27 18:06:01 -0700

    added is_lct_dir, not all functionality finalized

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,11 @@
 #Licensing
 
+#imports
+import os.path
+import os
+
 #Utils for creating LCT Directory
+
 
 #Creates top level lct directory structure at "path"
 def create_lct_directory(path, name):
@@ -57,3 +62,66 @@ def is_lct_directory(path):
     Returns:
         Boolean: True or False
     """
+
+    #individual verification bools
+    cameras_exist = os.path.exists(os.path.join(path, "cameras"))
+    inside_cameras_valid = check_inside_cameras(os.path.join(path, "cameras"))
+    pointcloud_exists = os.path.exists(os.path.join(path, "pointcloud"))
+    inside_pointcloud_valid = check_inside_pointcloud(os.path.join(path, "pointcloud"))
+    bounding_exists = os.path.exists(os.path.join(path, "bounding"))
+    ego_exists = os.path.exists(os.path.join(path, "ego"))
+
+    #overall verification bool
+    is_verified = True
+
+    #provides feedback to the user
+    if not cameras_exist:
+        print("There is no directory named \"cameras\" at the selected path.\n")
+        is_verified = False
+    if not inside_cameras_valid:
+        is_verified = False
+    if not pointcloud_exists:
+        print("There is no directory named \"pointcloud\" at the selected path. \n")
+        is_verified = False
+    if not inside_pointcloud_valid:
+        is_verified = False
+    if not bounding_exists:
+        print("There is no directory named \"bounding\" at the selected path. \n")
+        is_verified = False
+    if not ego_exists:
+        print("There is no directory named \"ego\" at the selected path. \n")
+        is_verified = False
+    
+    return is_verified
+
+
+#TODO implement
+#Checks to make sure that all the subdirectories of cameras only have Extrinsic.json, Intrinsic.json and .jpg files
+#Parameter is the path to the cameras dir
+#Returns false if not valid and true otherwise
+#Will print out reason for invalidity if one exists
+def check_inside_cameras(path):
+    for dir in os.listdir(path):
+        print()
+    
+    return True
+
+#TODO implement
+#Checks to make sure that all the subdirectories of cameras only have .pcd files
+#Returns false if not valid and true otherwise
+#Will print out reason for invalidity if one exists
+def check_inside_pointcloud(path):
+    print()
+    return true
+
+
+def main():
+    answer = is_lct_directory("/home/avetter/Desktop/Dev/lidar/src")
+
+    if answer:
+        print("it works")
+    else:
+        print("doesnt work")
+
+if __name__ == "__main__":
+    main()
