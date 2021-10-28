a4aedb56f705e2a5f004ba5043d18c605ec5ec20
mbetberg <mbetberg@gmail.com>
2021-10-27 19:06:06 -0700

    Split the method into create_rgb_sensor_directory and add_rgb_frame. Also removed debugging code.

--- src/utils.py
+++ src/utils.py
@@ -17,50 +17,63 @@ def create_lct_directory(path, name):
         None
     """
 
-def create_rgb_sensor_directory(path, name, images, translation, rotation, intrinsic):
-    """Adds one RGB sensor directory inside camera directory
+def create_rgb_sensor_directory(path, name, translation, rotation, intrinsic):
+    """Adds one RGB sensor directory inside camera directory, and adds extrinsic/intrinsic data for said sensor
     Args:
         path: path to LCT directory
         name: name of RGB sensor
-        images: list of buffers containing JPG images (assumed that length of list is also number of frames)
         translation: (x,y,z) tuple representing sensor translation
         rotation: (w,x,y,z) quaternion representing sensor rotation
         intrinsic: [3,3] 3x3 2D List representing intrinsic matrix
     Returns:
         None
         """
-    if(path[-1] in "\\/"):
-        path = path[:-1]
 
+    #Create necessary directories: ./Cameras and ./Cameras/[name]. These directories are needed for the
+    #file, so the program creates them.
     try:
         os.mkdir(os.path.join(path, "Cameras"))
     except FileExistsError:
-        print("Path exists")
+        pass
     
     work_dir = os.path.join(os.path.join(path, "Cameras"), name)
     try:
         os.mkdir(work_dir)
     except FileExistsError:
-        print("Camera exists")
+        pass
 
-
+    #Creates the Extrinsic.json file in the /Cameras/[name] directory from the
+    #translation and rotation parameters.
     extrinsics = {}
     extrinsics['translation'] = translation
     extrinsics['rotation'] = rotation
 
 
-    extrinsic_file = open(work_dir + "/Extrinsics.json", "w")
+    extrinsic_file = open(work_dir + "/extrinsics.json", "w")
     extrinsic_file.write(json.dumps(extrinsics))
     extrinsic_file.close()
 
-    intrinsic_file = open(work_dir + "/Intrinsics.json", "w")
+
+    #Creates the Extrinsic.json file in the /Cameras/[name] directory from the intrinsic parameter.
+    intrinsic_file = open(work_dir + "/intrinsics.json", "w")
     intrinsic_file.write(json.dumps({"matrix" : intrinsic}))
     intrinsic_file.close()
 
-    i = 0
-    for image in images:
-        image.save(os.path.join(work_dir, str(i) +".jpg"))    
-        i += 1
+
+def add_rgb_frame(path, name, image, frame):
+    """Adds one jpg from one frame to the structure inside the camera directory for a given sensor
+    Args:
+        path: path to LCT directory
+        name: name of RGB sensor
+        images: list of buffers containing JPG images (assumed that length of list is also number of frames)
+        frame: the number corresponding to the frame
+    Returns:
+        None
+        """
+
+    #Assumes directory exists
+    image.save(os.path.join(os.path.join(os.path.join(path, "Cameras"), name),
+    str(frame) +".jpg"))
 
 
 def create_lidar_sensor_directory(path, name, images, translation, rotation):
@@ -103,11 +116,18 @@ def import_waymo_debug(filepath):
 
     dataset = tf.data.TFRecordDataset(os.path.join(filepath,"training0.tfrecord"),'')
 
+
     rgb = True
+    image_arr = []
+    i = 3
     for data in dataset:
+            i -= 1
             frame = open_dataset.Frame()
             frame.ParseFromString(bytearray(data.numpy()))
-            break
+            image = PIL.Image.open(io.BytesIO(frame.images[0].image))
+            image_arr.append(image)
+            if(not(i)):
+                break
 
     if rgb:
         #We wrap the raw jpg data using BytesIO to avoid saving a temporary file using BytesIO
@@ -116,7 +136,7 @@ def import_waymo_debug(filepath):
         #convert the  PIL image object to a numpy array using asarray(), since that is what open3d expects
         #image = o3d.geometry.Image(np.asarray(image))
     
-    return image
+    return image_arr
 
 if (__name__ == "__main__"):
 
@@ -131,5 +151,11 @@ if (__name__ == "__main__"):
     except OSError as e:
         pass
 
-    create_rgb_sensor_directory(dir_path, "Camera1", [img, img], (1, 2, 3),
+    create_rgb_sensor_directory(dir_path, "Camera1", (1, 2, 3),
     (1, 2, 3, 4), [[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+    fram = 0
+    for i in img:
+        add_rgb_frame(dir_path, "Camera1", i, fram)
+        fram += 2
+
