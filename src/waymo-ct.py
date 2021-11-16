2fe6940f6cc1fbf399a6a28764a806dde818aba9
Ben Namovicz <brnamovicz@gmail.com>
2021-11-16 18:02:22 -0500

    Commented code with consistent format

--- src/waymo-ct.py
+++ src/waymo-ct.py
@@ -11,7 +11,7 @@ import PIL
 import io
 
 
-# Name of all the cameras
+# Name of all the RGB cameras
 RGB_Name  = {
     0:"UNKNOWN",
     1:'FRONT',
@@ -21,6 +21,7 @@ RGB_Name  = {
     5:'SIDE_RIGHT'
   }
 
+# Name of all LiDAR sensors
 Lidar_Name = {
     0:"UNKNOWN",
     1:"TOP",
@@ -29,7 +30,6 @@ Lidar_Name = {
     4:"SIDE_RIGHT",
     5:"REAR"
 }
-
 
 
 #Get command line options
@@ -61,12 +61,22 @@ def parse_options():
     return (waymo_path, output_path, custom_path)
 
 def extract_bounding(frame, frame_num, lct_path):
+    """Extracts the bounding data from a waymo frame and converts it into our intermediate format
+    Args:
+        frame: waymo frame
+        frame_num: frame number
+        lct_path: path to LCT directory
+    Returns:
+        None
+        """
+
     origins = []
     sizes = []
     rotations = []
     annotation_names = []
     annotation_dict = {1: "Vehicle", 2: "Pedestrian", 3: "Sign", 4:"Cyclist"}
     confidences = []
+
     for label in frame.laser_labels:
         origins.append(np.transpose(np.matmul(np.array(frame.pose.transform).reshape((4, 4)), np.array(
             [[label.box.center_x], [label.box.center_y], [label.box.center_z], [1]]))).tolist()[0][:3])
@@ -74,14 +84,14 @@ def extract_bounding(frame, frame_num, lct_path):
         annotation_names.append(annotation_dict[label.type])
         rotations.append([0,0,0,0])
         confidences.append(100)
-    utils.create_frame_bounding_directory(lct_path, frame_num, origins, sizes,rotations,annotation_names,confidences)
 
-def setup_rgb(frame, output_path):
+    utils.create_frame_bounding_directory(lct_path, frame_num, origins, sizes, rotations, annotation_names, confidences)
 
+def setup_rgb(frame, lct_path):
     """Sets up the RGB directory with extrinsic data
     Args:
         frame: waymo frame
-        output_path: path to LCT directory
+        lct_path: path to LCT directory
     Returns:
         None
         """
@@ -102,24 +112,33 @@ def setup_rgb(frame, output_path):
     
     for image in frame.images:
         translation, rotation_quats = utils.translation_and_rotation(camera_data_ext[RGB_Name[image.name]].tolist())
-        utils.create_rgb_sensor_directory(output_path, RGB_Name[image.name], translation, rotation_quats, camera_data_int[RGBName[image.name]])
+        utils.create_rgb_sensor_directory(lct_path, RGB_Name[image.name], translation, rotation_quats, camera_data_int[RGBName[image.name]])
 
-def extract_rgb(frame, frame_num):
-
+def extract_rgb(frame, frame_num, lct_path):
     """Extracts the RGB data from a waymo frame and converts it into our intermediate format
     Args:
         frame: waymo frame
         frame_num: frame number
+        lct_path: path to LCT directory
     Returns:
         None
         """
 
     #create the directory and files:
     for image in frame.images:
-        utils.add_rgb_frame(output_path, RGB_Name[image.name], PIL.Image.open(io.BytesIO(image.image)), frame_num)
+        utils.add_rgb_frame(lct_path, RGB_Name[image.name], PIL.Image.open(io.BytesIO(image.image)), frame_num)
 
-#Uses the first frame to initialize 
 def setup_lidar(frame, lct_path, translations, rotations):
+    """Uses the first frame to initialize LiDAR directory and store extrinsic data
+    Args:
+        frame: waymo frame
+        lct_path: path to LCT directory
+        translations: empty translation dictionary
+        rotations: empty rotation dictionary
+    Returns:
+        None
+        """
+
     calibrations = sorted(frame.context.laser_calibrations, key=lambda c: c.name)
     for c in calibrations:
         sensor = c.name
@@ -133,9 +152,18 @@ def setup_lidar(frame, lct_path, translations, rotations):
         translations[sensor] = translation
         rotations[sensor] = rotation
 
-#Extracts LiDAR data from one frame and puts it in the lct file system
 def extract_lidar(frame, frame_num, lct_path, translations, rotations):
-    
+    """Extracts LiDAR data from one frame and puts it in the lct file system
+    Args:
+        frame: waymo frame
+        frame_num: frame number
+        lct_path: path to LCT directory
+        translations: translation dictionary
+        rotations: rotation dictionary
+    Returns:
+        None
+        """
+
     #Extract the pointclouds as a list of points
     range_images, camera_projections,range_image_top_pose = frame_utils.parse_range_image_and_camera_projection(frame)
     point_clouds, cp_points = frame_utils.convert_range_image_to_point_cloud(frame,range_images,camera_projections,range_image_top_pose,0,False)
@@ -167,7 +195,7 @@ if __name__ == "__main__":
         utils.create_lct_directory(os.getcwd(), output_path)
 
     #Extract data from TFRecord File
-    dataset = tf.data.TFRecordDataset(waymo_path,'')
+    dataset = tf.data.TFRecordDataset(waymo_path, '')
 
     #Initialize LiDAR camera dictionarys
     translations = {}
@@ -177,12 +205,14 @@ if __name__ == "__main__":
     for frame_num, data in enumerate(dataset):
         frame = open_dataset.Frame()
         frame.ParseFromString(bytearray(data.numpy()))
+
         if frame_num == 0:
             setup_rgb(frame, output_path)
             setup_lidar(frame, output_path, translations, rotations)
+        
         #At this point have one frame imported as 'frame'
-        extract_bounding(frame,frame_num,output_path)
-        extract_rgb(frame, frame_num)
+        extract_bounding(frame, frame_num, output_path)
+        extract_rgb(frame, frame_num, output_path)
         extract_lidar(frame, frame_num, output_path, translations, rotations)
         extract_ego(frame, frame_num, output_path)
         
