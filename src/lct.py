ad18ced108c127c3ba5af9fd06d74d0dda72cb27
ajedgley <ajedgley@terpmail.umd.edu>
2022-11-09 16:40:17 -0500

    Tweaked LCT internal add_box function. Will migrate functions back to Annotation class soon

--- src/lct.py
+++ src/lct.py
@@ -9,6 +9,8 @@ import matplotlib.colors
 import matplotlib.pyplot as plt
 from PIL import Image
 import sys
+import functools
+import random
 import numpy as np
 import open3d.visualization.gui as gui
 from PIL import Image
@@ -70,7 +72,7 @@ def parse_options():
 
 class Window:
     MENU_IMPORT = 1
-    def __init__(self, lct_dir):        
+    def __init__(self, lct_dir):
         np.set_printoptions(precision=15)
 
         # Create the objects for the 3 windows that appear when running the application
@@ -89,7 +91,13 @@ class Window:
         self.show_false_positive = False
         self.show_incorrect_annotations = False
         self.boxes_to_render = []
-        self.boxes_in_scene = []
+        #the below 5 variables involved in selecting and adjusting annotations
+        self.box_indices = []
+        self.volumes_rendered = []
+        self.box_selected = None
+        self.previous_index = -1
+        self.box_count = 0
+        self.coord_frame = "coord_frame"
         self.show_score = False
         self.label_list = []
         # These three values represent the current LiDAR sensors, RGB sensors, and annotations being displayed
@@ -144,7 +152,7 @@ class Window:
         self.widget3d = gui.SceneWidget()
         self.widget3d.scene = rendering.Open3DScene(pw.renderer)
         self.widget3d.scene.set_background([0,0,0,255])
-        self.mat = rendering.Material()
+        self.mat = rendering.MaterialRecord()
         self.mat.shader = "defaultUnlit"
         self.mat.point_size = 2
         #self.mat.base_color = [255,255,255,255]
@@ -601,7 +609,7 @@ class Window:
         self.widget3d.scene.add_geometry("Point Cloud", self.pointcloud, self.mat)
         self.widget3d.scene.show_axes(True)
         i = 0
-        mat = rendering.Material()
+        mat = rendering.MaterialRecord()
         mat.shader = "unlitLine"
         mat.line_width = .25
 
@@ -617,8 +625,8 @@ class Window:
             bounding_box.translate(np.array(self.frame_extrinsic['translation']))
             hex = '#%02x%02x%02x' % color # bounding_box.color needs to be a tuple of floats (color is a tuple of ints)
             bounding_box.color = matplotlib.colors.to_rgb(hex)
-            self.boxes_in_scene.append(bounding_box)
 
+            self.box_indices.append(box[ANNOTATION] + str(i)) #used to reference specific boxes in scene
             if box[CONFIDENCE] < 100 and self.show_score:
                 label = self.widget3d.add_3d_label(bounding_box.center, str(box[CONFIDENCE]))
                 label.color = gui.Color(1.0,0.0,0.0)
@@ -1043,7 +1051,7 @@ class Window:
     	
     	# closes them for now, just for convenience. Maybe not necessary final build, we'll see
     	self.controls.close()
-    	annotation_object = edit.Annotation(self.widget3d, self.pointcloud_window, self.frame_extrinsic, self.boxes, self.path_string)
+    	annotation_object = self.setup_control_window(self.widget3d, self.pointcloud_window, self.frame_extrinsic, self.boxes, self.path_string)
 
 	# self.widget3d
 
@@ -1085,6 +1093,240 @@ class Window:
             lidar_sensors.append(lidar_name)
 
         return (camera_sensors, lidar_sensors)
+
+    def setup_control_window(self, scene_widget, pointcloud, frame_extrinsic, boxes, path):
+        cw = gui.Application.instance.create_window("LCT", 400, 800)
+
+        # modify temp boxes in this file, then when it's time to save use them to overwrite existing json
+        temp_boxes = boxes.copy()
+        self.create_box_scene(self.widget3d, self.boxes_to_render, self.frame_extrinsic)
+
+        boxes_available = []
+        # shamelessly stolen from lct setup, cuz their window looks nice
+        em = cw.theme.font_size
+        margin = gui.Margins(0.50 * em, 0.25 * em, 0.50 * em, 0.25 * em)
+        layout = gui.Vert(0, margin)
+
+        # button for adding a new bounding box
+        add_box_horiz = gui.Horiz()
+        add_box_button = gui.Button("Add New Bounding Box")
+        box_partial = functools.partial(self.add_bounding_box, widget=scene_widget, pw=pointcloud,
+                                        fe=frame_extrinsic)  # necessary to add args to functions
+        add_box_button.set_on_clicked(box_partial)
+        add_box_horiz.add_child(add_box_button)
+
+        # button for saving annotation changes
+        save_annotation_horiz = gui.Horiz()
+        save_annotation_button = gui.Button("Save Changes")
+        save_partial = functools.partial(self.save_changes_to_json, temp_boxes=temp_boxes, path=path)
+        save_annotation_button.set_on_clicked(save_partial)
+        save_annotation_horiz.add_child(save_annotation_button)
+
+        # button for save as, textbox for save as location
+        # on hold because text boxes don't really work with partials
+        """
+        save_as_horiz = gui.Horiz()
+    
+        save_as_textbox = gui.TextEdit()
+        save_as_textbox.placeholder_text = path
+    
+        save_as_button = gui.Button("Save As...")
+        save_as_partial = functools.partial(save_as, temp_boxes=temp_boxes, path=save_as_textbox.text_value)
+        save_as_button.set_on_clicked(save_as_partial)
+    
+        save_as_horiz.add_child(save_as_button)
+        save_as_horiz.add_child(save_as_textbox) """
+
+        # button for exiting annotation mode
+        exit_annotation_horiz = gui.Horiz()
+        exit_annotation_button = gui.Button("Exit Annotation Mode")
+        exit_partial = functools.partial(self.exit_annotation_mode, widget=scene_widget)
+        exit_annotation_button.set_on_clicked(exit_partial)
+        exit_annotation_horiz.add_child(exit_annotation_button)
+
+        # add various metrics and number thingies used to display info about the current bbox
+        click_partial = functools.partial(self.get_point_depth, widget=scene_widget, boxes=boxes_available)
+        scene_widget.set_on_mouse(click_partial)
+
+        # adding all of the horiz to the vert, in order
+        layout.add_child(add_box_horiz)
+        layout.add_child(save_annotation_horiz)
+        # layout.add_child(save_as_horiz)
+        layout.add_child(exit_annotation_horiz)
+
+        cw.add_child(layout)
+
+        return cw
+
+
+    # function should:
+    # disable the current mouse functionality
+    # onclick, adds a bounding box at the location of click (or maybe just center screen? idk)
+    # highlight the current bounding box
+    # re-enable the mouse functionality
+    # return the new bounding box
+    def add_bounding_box(self, widget, pw, fe):
+        partial = functools.partial(self.place_bounding_box, widget=widget, pw=pw, fe=fe)
+        widget.set_on_mouse(partial)
+
+
+    # function should:
+    # close the exiting control panel
+    # idk, restore the state as if the program just reopened
+    # potential ideas:
+    # -just restart the program (hey, it'll probably work)
+    # -close control window, reopen any previously closed windows, and run update (maybe update done in lct)
+    def exit_annotation_mode(self, widget):
+        # os.execl(sys.executable, os.path.abspath(__file__), *sys.argv), this doesn't work but maybe it's an idea
+        print("TODO")
+        widget.set_on_mouse(self.enable_mouse)
+
+
+    # onclick, places down a bounding box on the cursor, then reenables mouse functionality
+    def place_bounding_box(self, event, widget, pw, fe):
+        if event.type == gui.MouseEvent.Type.BUTTON_DOWN:
+            # Random values are placeholders until we implement the desired values
+            qtr = Quaternion([random.uniform(-0.1, 1), random.uniform(-0.1, 1), random.uniform(-0.1, 1),
+                              random.uniform(0, 1)])  # Randomized rotation of box
+            origin = self.widget3d.center_of_rotation# Random origin of box within existing pointcloud bounds
+            size = [random.randint(1, 5), random.randint(1, 5), random.randint(1, 5)]  # Random dimensions of box
+            mat = rendering.MaterialRecord()
+
+            bounding_box = o3d.geometry.OrientedBoundingBox(origin, qtr.rotation_matrix,
+                                                            size)  # Creates bounding box object
+            #bounding_box.rotate(Quaternion(fe['rotation']).rotation_matrix,
+            #                    [0, 0, 0])  # Frame extrinsic data necessary to display boxes
+            #bounding_box.translate(np.array(fe['translation']))
+            bounding_box.color = matplotlib.colors.to_rgb(
+                (0, 0, 1))  # Custom bounding boxes are blue to differentiate from the existing ones
+
+            #global box_count  # Ensures that the scene treats each box differently, letting you add multiple
+            widget.scene.add_geometry(str(self.box_count), bounding_box, mat)  # Generates a box
+
+            widget.force_redraw()
+            pw.post_redraw()
+            print("clicked!")
+            self.box_count += 1
+
+            widget.set_on_mouse(self.enable_mouse)
+            return gui.Widget.EventCallbackResult.CONSUMED
+
+        return gui.Widget.EventCallbackResult.CONSUMED
+
+
+    # disables current mouse functionality, ie dragging screen and stuff
+    def disable_mouse(self, event):
+        return gui.Widget.EventCallbackResult.CONSUMED
+
+
+    # re-enables mouse functionality to their defaults
+    def enable_mouse(self, event):
+        return gui.Widget.EventCallbackResult.IGNORED
+
+
+    def get_point_depth(self, event, widget, boxes):
+
+      if event.type == gui.MouseEvent.Type.BUTTON_DOWN and event.is_modifier_down(gui.KeyModifier.CTRL):
+            def get_depth(depth_image):
+                x = event.x - widget.frame.x
+                y = event.y - widget.frame.y
+
+                depth = np.asarray(depth_image)[y, x]
+                world = widget.scene.camera.unproject(
+                    event.x, event.y, depth, widget.frame.width, widget.frame.height)
+                output = "({:.3f}, {:.3f}, {:.3f})".format(
+                    world[0], world[1], world[2])
+
+                print(output)
+                get_nearest(world)
+
+            def get_nearest(world_coords):
+                boxes = self.volumes_rendered
+                if len(boxes) != 0:
+                    smallest_dist = np.linalg.norm(world_coords - boxes[0].get_center())
+                    closest_box = boxes[0]
+                    for box in boxes:
+                        curr_dist = np.linalg.norm(world_coords - box.get_center())
+                        if curr_dist < smallest_dist:
+                            smallest_dist = curr_dist
+                            closest_box = box
+
+                    closest_index = boxes.index(closest_box)
+                    self.box_selected = self.box_indices[closest_index]
+                    self.select_box(closest_index)
+
+
+            widget.scene.scene.render_to_depth_image(get_depth)
+            return gui.Widget.EventCallbackResult.HANDLED
+      return gui.Widget.EventCallbackResult.IGNORED
+
+    def select_box(self, box_index):
+        if self.previous_index != -1: #if not first box clicked "deselect" previous box
+            prev_mat = rendering.MaterialRecord()
+            prev_mat.shader = "unlitLine"
+            prev_mat.line_width = 0.25
+            rendering.Open3DScene.modify_geometry_material(self.widget3d.scene, self.box_indices[self.previous_index],
+                                                           prev_mat)
+
+        rendering.Open3DScene.remove_geometry(self.widget3d.scene, self.coord_frame)
+        self.previous_index = box_index
+        box = self.box_indices[box_index]
+        origin = o3d.geometry.TriangleMesh.get_center(self.volumes_rendered[box_index])
+        frame = o3d.geometry.TriangleMesh.create_coordinate_frame(1.0, origin)
+        frame_mat = rendering.MaterialRecord()
+        frame_mat.shader = "defaultUnlit"
+        mat = rendering.MaterialRecord()
+        mat.shader = "unlitLine"
+        rendering.Open3DScene.modify_geometry_material(self.widget3d.scene, box, mat)
+        self.widget3d.scene.add_geometry("coord_frame", frame, frame_mat, True)
+
+    # overwrites currently open file with temp_boxes
+    def save_changes_to_json(temp_boxes, path):
+        with open(path, "w") as outfile:
+            outfile.write(json.dumps(temp_boxes))
+
+
+    # prompt with user input for new path, then saves
+    def save_as(self, temp_boxes, path):
+        print(path)
+        self.save_changes_to_json(temp_boxes, path)
+
+
+    def create_box_scene(self, scene, boxes, extrinsics):
+        id = 0
+        cube_indices = []
+        boxes_to_return = []
+        mat = rendering.MaterialRecord()
+        mat.shader = "defaultLitTransparency"
+        mat.base_color = (0.0, 0.0, 0.0, 0.0) #sets boxes to be invisble
+
+
+        coord_frame_mat = rendering.MaterialRecord()
+        coord_frame_mat.shader = "defaultLit"
+        frame_to_add = o3d.geometry.TriangleMesh.create_coordinate_frame()
+        scene.scene.add_geometry("coord_frame", frame_to_add, coord_frame_mat, False)
+        for box in boxes:
+            size = [0, 0, 0]
+            size[0] = box[SIZE][1]
+            size[1] = box[SIZE][0]
+            size[2] = box[SIZE][2]
+
+            cube_to_add = o3d.geometry.TriangleMesh.create_box(size[0], size[1], size[2], False, False)
+            cube_to_add = cube_to_add.translate(np.array([0, 0, 0]), False)
+            cube_to_add = cube_to_add.rotate(Quaternion(box[ROTATION]).rotation_matrix, [0, 0, 0])
+            cube_to_add = cube_to_add.translate(box[ORIGIN])
+
+            cube_to_add = cube_to_add.rotate(Quaternion(extrinsics['rotation']).rotation_matrix, [0, 0, 0])
+            cube_to_add = cube_to_add.translate(np.array(extrinsics['translation']))
+            cube_id = "cube_" + str(id)
+            cube_indices.append(cube_id)
+            id += 1
+
+            cube_to_add.compute_vertex_normals()
+            boxes_to_return.append(cube_to_add)
+            scene.scene.add_geometry(cube_id, cube_to_add, mat)
+
+        self.volumes_rendered = boxes_to_return
 
 
 if __name__ == "__main__":
